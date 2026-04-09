cd2cf353aafe7b044c68d3f5c3356e9f5a8622bc
Dakitidunno <juanchojuan6@hotmail.com>
2026-04-09 11:03:42 -0500

    Typo fixed

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -8,7 +8,7 @@ return(
    <>
    <h2 id="aboutMe">About Me</h2>
    <section className="aboutMe">
-          <p>Hi, I'm Dakiti!  Welcome to my journey in becoming a software architect. Now I am giving all my time to get better at Front-end skills before I move on Back-end and databases. I've been learning using AI (using socratic mode as instructions for my AI), I think AI is Google on steroids, so it helps me to search a lot of info in this big library called internet (yes I know I have to be cautios and critic with the information we have, we are in the age of information lol), since I am learning and I am curious about what's under the hood, I like to know how the things work before I got into, that helps me to get better answers and solutions. On the other hand, I really enjoy knowing new people, I think it helps to know more about the world we are on.
+          <p>Hi, I'm Dakiti!  Welcome to my journey in becoming a software architect. Now I am giving all my time to get better at Front-end skills before I move on Back-end and databases. I've been learning using AI (using socratic mode as instructions for my AI), I think AI is Google on steroids, so it helps me to search a lot of info in this big library called internet (yes I know I have to be cautious and critic with the information we have, we are in the age of information lol), since I am learning and I am curious about what's under the hood, I like to know how the things work before I got into, that helps me to get better answers and solutions. On the other hand, I really enjoy knowing new people, I think it helps to know more about the world we are on.
 
             <br />
             <br />
